c9f67837077a27aa8e91a46ee0fcd6deb4f315e3
natasciarufino <natasciarufino17@gmail.com>
2025-01-21 17:23:54 +0100

    update

---
+++ newProject/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from './Hello';  // Importiamo il componente Hello
+
+function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
